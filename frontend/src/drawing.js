8f9cdd905a0b9ca8846857682f0f1065cbeccc65
LingChen <chenlingnk@gmail.com>
2018-11-03 20:32:53 -0500

    update frontend vandy hacks svg

--- frontend/src/drawing.js
+++ frontend/src/drawing.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import MtSvgLines from 'react-mt-svg-lines';  
 import SVG from 'react-inlinesvg';
-import vandyhacklogo from './assets/vandyhack.logo.svg'
+//import vandyhacklogo from './assets/vandyhack.logo.svg'
+import vandyhacklogo from './assets/vhv.svg'
 
 
 class DrawArea extends React.Component {
@@ -52,22 +53,34 @@ class DrawArea extends React.Component {
             if (this.props.EnteredText.includes('hack') | this.props.EnteredText.includes('vand')) {
                 return(          
                      <div className='hacklogo'>
-                        <SVG className='hacklogosvg' src={vandyhacklogo}></SVG>
+                        {/*<SVG className='hacklogosvg' src={vandyhacklogo}></SVG> */}
+                        <svg className='hacklogosvg' fill='none' viewbox="0 0 1000 1000">
+                            <path stroke='black' strokeWidth='8' d="M7.631 0 L88.194 179 L169.29 0 M32.363 0 L103 142 M190 0 L150 84 M142 104 L103 189 M210 0 L120 189"/>
+                        </svg>
                     </div>
                     );
             } 
             
             else {
-                return (
-                    <div className='drawarea'>
-                    <MtSvgLines animate={ true } duration={ 2000 }>
-                        <svg className='drawsvg' x="0" y="0" viewBox="0 0 500 500" >
-                            {/* <path d={this.state.svg_path}/> */}
-                            {this.state.svg_path.map(item => (<path d={item} />))}
-                        </svg>
-                    </MtSvgLines>
-                    </div>
-                );
+                try {
+                    return (
+                        <div className='drawarea'>
+                        <MtSvgLines animate={ true } duration={ 2000 }>
+                            <svg className='drawsvg' x="0" y="0" viewBox="0 0 500 500" >
+                                {/* <path d={this.state.svg_path}/> */}
+                                {this.state.svg_path.map(item => (<path d={item} />))}
+                            </svg>
+                        </MtSvgLines>
+                        </div>
+                    );
+                 } catch (e) {
+                     // catch backend not running problem
+                    return(
+                    <div className='hacklogo'>
+                        <SVG className='hacklogosvg' src={vandyhacklogo}></SVG>
+                    </div> 
+                    );
+                }
             }
         }
     }
